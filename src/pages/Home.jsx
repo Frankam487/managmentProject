6223997ec697c9ced6a61cbbc00df55080c3120c
Frank kamagang <fkamgang06@gmail.com>
2024-08-13 13:14:27 +0200

    environments config

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h1>home</h1>
+        </div>
+     );
+}
+ 
+export default Home;
+<div className="home">
+    <h1>home</h1>
+</div>
