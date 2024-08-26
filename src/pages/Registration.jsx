4de04841a1c034b0b87bc482c7a124d92dbadb92
Frank kamagang <fkamgang06@gmail.com>
2024-08-26 23:00:36 +0200

    form-registration

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import { Logo } from "../components/Logo";
 
 export const Registration = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
     return (
         <div className="registration">
             <Logo />
             <div className="registration-content">
                 <form autoComplete="off">
                     <label htmlFor="email">Nom:</label>
-                    <input type="text" autoComplete="off" id="name" placeholder="entrer votre nom" />
+                    <input type="text" onChange={(e) => setName(e.target.value)} autoComplete="off" id="name" placeholder="entrer votre nom" />
                     <label htmlFor="email">Email:</label>
-                    <input type="email" autoComplete="off" id="email" placeholder="entrer email" />
+                    <input type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        autoComplete="off" id="email" placeholder="entrer email" />
                     <label htmlFor="password">password</label>
-                    <input autoComplete="off" type="password" placeholder="enter your password" id="password" />
+                    <input
+                        onChange={(e) => setPassword(e.target.value)} autoComplete="off" type="password" placeholder="enter your password" id="password" />
+                    <label htmlFor="password">Confirm-password</label>
+                    <input
+                        onChange={(e) => setConfirm(e.target.value)}
+                        autoComplete="off" type="password" placeholder="enter your password" id="password" />
                     <button>Login</button>
                 </form>
             </div>
