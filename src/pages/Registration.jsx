7a0224c9323fde60caea5ee7387b1d952bf7f264
Frank kamagang <fkamgang06@gmail.com>
2024-08-27 00:34:10 +0200

    react-hook-form

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,30 +1,38 @@
 import { useState } from "react";
 import { Logo } from "../components/Logo";
-
+import { useForm } from 'react-hook-form'
 export const Registration = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirm, setConfirm] = useState("");
+    const { register, handleSubmit, formState: {errors} } = useForm();
+
+
+    const onsubmit = (e) => {
+        console.log(e);
+
+    }
     return (
         <div className="registration">
             <Logo />
             <div className="registration-content">
-                <form autoComplete="off">
-                    <label htmlFor="email">Nom:</label>
-                    <input type="text" onChange={(e) => setName(e.target.value)} autoComplete="off" id="name" placeholder="entrer votre nom" />
-                    <label htmlFor="email">Email:</label>
-                    <input type="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        autoComplete="off" id="email" placeholder="entrer email" />
+                <form onSubmit={handleSubmit(onsubmit)} autoComplete="off">
+                    <label htmlFor="email">Nom:</label> <br />
+                    <input type="text" autoComplete="off" 
+                    {...register("name")} id="name" name="name" placeholder="entrer votre nom" /> <br />
+
+                    <label htmlFor="email">Email: <br /></label>
+
+                    <input type="email" name="email"
+                        {...register("email")}
+                        autoComplete="off" id="email" placeholder="entrer email" /> <br />
                     <label htmlFor="password">password</label>
+                    <br />
                     <input
-                        onChange={(e) => setPassword(e.target.value)} autoComplete="off" type="password" placeholder="enter your password" id="password" />
+                        autoComplete="off" type="password" name="password" placeholder="enter your password" id="password" {...register(password)} /> <br />
                     <label htmlFor="password">Confirm-password</label>
+                    <br />
                     <input
-                        onChange={(e) => setConfirm(e.target.value)}
-                        autoComplete="off" type="password" placeholder="enter your password" id="password" />
-                    <button>Login</button>
+                        autoComplete="off" type="password" placeholder="enter your password" name="confirm" id="password" {...register("confirm")} />
+                    <br /> <br />
+                    <button type="submit">Login</button>
                 </form>
             </div>
         </div>
