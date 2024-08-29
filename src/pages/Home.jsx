07c07a6774c3094d8a22b7bd50a391e49832afe4
Frank kamagang <fkamgang06@gmail.com>
2024-08-29 22:21:24 +0200

    verification user sign in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,43 @@
 import { Link } from "react-router-dom";
 import { NavLogin } from "../components/NavLogin";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Home = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const signIn = (e) => {
+        const email = e.email;
+        const password = e.password;
+        axios.get("http://localhost:2001/users").then((res) => {
+            const datas = res.data;
+            console.log(res.data);
+            
+            const users = datas.find((user) => user.email === email && user.password===password)
+            if(users){
+                alert('ok')
+            } else{
+                alert('nn!')
+            }
+        }
+        )
+    }
     return (
         <div className="home">
-           <div className="home-header">
-           <h1 className="bi bi-house">Manager</h1>
-           <NavLogin />
-           </div>
+            <div className="home-header">
+                <h1 className="bi bi-house">Manager</h1>
+                <NavLogin />
+            </div>
             <div className="login-container">
                 <h1>Login</h1>
-                <form autoComplete="off">
+                <form autoComplete="off" onSubmit={handleSubmit(signIn)}>
                     <label htmlFor="email">email:</label>
-                    <input type="text" autoComplete="off" id="email" placeholder="enter your email" />
+                    <input type="text" autoComplete="off" id="email" {...register("email", { required: true })} name="email"
+                        laceholder="enter your email" />
+                    {errors.email && <p style={{ color: "red" }}>Email requis</p>}
                     <label htmlFor="password">password</label>
-                    <input autoComplete="off" type="password" placeholder="enter your password" id="password" />
-                    <button>Login</button>
+                    <input autoComplete="off" type="password" placeholder="enter your password" id="password" {...register("password", { required: true })} />
+                    {errors.password && <p style={{ color: "red" }}>Erreur mdp</p>}
+                    <button type="submit">Login</button>
                 </form>
             </div>
 
